b8b5fe62d6cb98b2d3e830eccd909da7e68822fe
CampAsAChamp <nickschneider101@gmail.com>
2020-10-14 16:37:12 -0700

    Adding logic to update the display and store values

--- Calculator/app.js
+++ Calculator/app.js
@@ -1,3 +1,5 @@
+const multiplyCode = "test";
+
 function add(x, y) {
     return x + y;
 }
@@ -66,8 +68,17 @@ function operate(operator, x, y) {
 
 let calcDisplay = document.querySelector("#calc-display");
 let numberBtns = document.querySelectorAll(".number");
+let operatorBtns = document.querySelectorAll(".operator");
 let clearAllBtn = document.querySelector("#clear");
 let backspaceBtn = document.querySelector("#backspace");
+// let divideBtn = document.querySelector("#divide");
+// let multiplyBtn = document.querySelector("#multiply");
+// let subtractBtn = document.querySelector("#subtract");
+// let addBtn = document.querySelector("#add");
+let firstNum = 0;
+let secondNum = 0;
+let operator = "";
+let opPressed = false;
 
 clearAllBtn.addEventListener("click", () => {
     calcDisplay.textContent = "";
@@ -79,6 +90,37 @@ backspaceBtn.addEventListener("click", () => {
 
 numberBtns.forEach((singleNum) => {
     singleNum.addEventListener("click", () => {
+        if (opPressed) {
+            calcDisplay.textContent = "";
+            secondNum = singleNum.textContent;
+            console.log(firstNum, operator, secondNum);
+            operate(operator, firstNum, secondNum);
+        }
         calcDisplay.textContent += singleNum.textContent;
+    });
+});
+
+/*
+1) Store the first number that is input into the calculator when a user presses an operator
+2) Save which operation has been chosen
+3) Call operate() on them when the user presses the “=” key.
+
+You should already have the code that can populate the display, 
+so once operate() has been called, update the display with the ‘solution’ to the operation.
+*/
+
+operatorBtns.forEach((singleOperator) => {
+    singleOperator.addEventListener("click", () => {
+        firstNum = calcDisplay.textContent;
+        operator = singleOperator.textContent;
+
+        // if (
+        // calcDisplay.textContent.length !== 0 &&
+        // calcDisplay.textContent.charAt(calcDisplay.textContent.length - 1) !== singleOperator.textContent
+        // ) {
+        opPressed = true;
+        console.log("Pressed an operator:", operator);
+        // calcDisplay.textContent += singleOperator.textContent;
+        // }
     });
 });
