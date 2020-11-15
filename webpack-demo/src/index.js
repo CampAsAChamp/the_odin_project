c643ce27a69265c3b72fcae2a5da7571888a2b27
CampAsAChamp <nickschneider101@gmail.com>
2020-11-15 09:29:45 -0800

    Added webpack and created first bundle

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,7 +1,10 @@
+// Explicitly requires lodash to be present, and binds it as _ (no global scope pollution)
+import _ from "lodash";
+
 function component() {
     const element = document.createElement("div");
 
-    // Lodash, currently included via a script, is required for this line to work
+    // Loadash, now imported by this script
     element.innerHTML = _.join(["Hello", "webpack"], " ");
 
     return element;
