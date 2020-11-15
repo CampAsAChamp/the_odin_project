e8c25564b5732f0e48aa6eb557c962d07b36fabf
CampAsAChamp <nickschneider101@gmail.com>
2020-11-15 12:25:12 -0800

    Added files for Webpack tutorial from TOP

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,13 +1,23 @@
 // Explicitly requires lodash to be present, and binds it as _ (no global scope pollution)
 import _ from "lodash";
+// Import our function
+import myName from "./myName";
 
 function component() {
+    var element = document.createElement("div");
+
+    // use your function!
+    element.innerHTML = myName("Cody");
+    return element;
+
+    /*
     const element = document.createElement("div");
 
     // Loadash, now imported by this script
     element.innerHTML = _.join(["Hello", "webpack"], " ");
 
     return element;
+    */
 }
 
 document.body.appendChild(component());
