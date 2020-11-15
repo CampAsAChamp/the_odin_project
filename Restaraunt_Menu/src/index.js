388d5a3207285a715f67d8c95401d6e6020ea575
CampAsAChamp <nickschneider101@gmail.com>
2020-11-15 14:17:35 -0800

    Finished adding tabs with clearing DOM

--- Restaraunt_Menu/src/index.js
+++ Restaraunt_Menu/src/index.js
@@ -2,8 +2,29 @@ import { homePageLoad } from "./homePageLoad";
 import { contactPageLoad } from "./contactPageLoad";
 import { menuPageLoad } from "./menuPageLoad";
 
-homePageLoad();
-// contactPageLoad();
-// menuPageLoad();
+function clearCurrentPageContent() {
+    const content = document.querySelector("#content");
+
+    while (content.firstChild) {
+        content.removeChild(content.lastChild);
+    }
+}
+
+const homeTab = document.querySelector("#home-tab");
+const menuTab = document.querySelector("#menu-tab");
+const contactUsTab = document.querySelector("#contact-us-tab");
+
+homeTab.addEventListener("click", () => {
+    clearCurrentPageContent();
+    homePageLoad();
+});
+menuTab.addEventListener("click", () => {
+    clearCurrentPageContent();
+    menuPageLoad();
+});
+contactUsTab.addEventListener("click", () => {
+    clearCurrentPageContent();
+    contactPageLoad();
+});
 
 console.log("Restaraunt page is working");
