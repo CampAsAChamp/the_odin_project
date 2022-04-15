744b117b65c7ddcd19ed8ffa04954f1bd47288c8
Nick Schneider <nickschneider101@gmail.com>
2022-04-14 17:32:43 -0700

    Finished task-app

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,11 +1,53 @@
 // Handle the input field with the logic
-// App.js
 
 import React, { Component } from "react";
+import Overview from './components/Overview';
+
 
 class App extends Component {
+  constructor() {
+    super();
+
+    // Declare state as an object 
+    this.state = {
+      task: '',
+      tasks: []
+    };
+
+    // Need to bind this function so it knows what context to operate in
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  // Updates the state's value every time we do something in the form
+  handleChange(event) {
+    // Can't change the event.target.value
+    this.setState({
+      task: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    if (this.state.task !== "") {
+      this.setState({
+        task: '',
+        tasks: [...this.state.tasks, this.state.task]
+      })
+      event.preventDefault();
+    }
+  }
+
   render() {
-    return <div>Hello World</div>;
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label> Input: </label>
+          <input type="text" name="input" value={this.state.task} onChange={this.handleChange} />
+          <button>Submit Task</button>
+        </form>
+        <Overview tasks={this.state.tasks} />
+      </div>
+    );
   }
 }
 
--- task-app/src/components/Overview.js
+++ task-app/src/components/Overview.js
@@ -1,1 +1,22 @@
 // Should render our tasks
+
+import React, { Component } from "react";
+import uniqid from "uniqid";
+
+
+class Overview extends Component {
+    render() {
+        const { tasks } = this.props;
+
+        return (
+            <ul>
+                {tasks.map((task) => {
+                    return <li key={uniqid()}>{task}</li>;
+                })}
+            </ul>
+        );
+    }
+}
+
+
+export default Overview;
