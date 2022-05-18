6492f1a0539f589475947e497996af10594938f9
Nick Schneider <nickschneider101@gmail.com>
2022-05-17 21:10:07 -0700

    Added data to populate

---
+++ shopping_cart/src/Data.js
@@ -0,0 +1,19 @@
+const Items = [
+  {
+    name: "RTX 3070",
+    description: "The 3nd best",
+    price: "500",
+  },
+  {
+    name: "RTX 3080",
+    description: "The 2nd best",
+    price: "700",
+  },
+  {
+    name: "RTX 3090",
+    description: "The very best",
+    price: "1500",
+  },
+];
+
+export default Items;
