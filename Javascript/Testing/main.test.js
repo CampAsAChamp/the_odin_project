2e623c40f9fb115041729db9b61c21772cce58d7
Nick Schneider <nickschneider101@gmail.com>
2022-05-23 20:26:33 -0700

    Added jest dom types

--- Javascript/Testing/main.test.js
+++ Javascript/Testing/main.test.js
@@ -3,6 +3,7 @@ import * as funcs from "./main";
 // 1. A capitalize function that takes a string and returns it with the first character capitalized.
 test("capitalize", () => {
   expect(funcs.capitalize("nick")).toBe("Nick");
+  expect;
 });
 
 // 2. A reverseString function that takes a string and returns it reversed.
